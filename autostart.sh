5883657442220457e40d79c62ef908e3cbb1ed5b
Mephres <58438219+MephresGH@users.noreply.github.com>
2022-04-25 17:56:24 +0200

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+nvidia-settings -a "GpuPowerMizerMode=1";
+nvidia-settings -a "DigitalVibrance=200";
+nvidia-settings -a '[gpu:0]/GPUGraphicsClockOffset[2]=120'; 
+nvidia-settings -a '[gpu:0]/GPUMemoryTransferRateOffset[2]=2000'
